b4ea6ee4234eeb538853331cc6bbd48aa56ba191
Ankulraja <154729128+Ankulraja@users.noreply.github.com>
2025-12-12 12:44:24 +0530

    change cors policy

--- server/index.js
+++ server/index.js
@@ -29,24 +29,8 @@ index.use(
 
 index.use(
   cors({
-    origin: function (origin, callback) {
-      const allowedOrigins = [
-        process.env.FRONTEND_URL,
-        process.env.FRONTEND_DEV_URL,
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-      ].filter(Boolean);
-
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.indexOf(origin) !== -1) {
-        return callback(null, true);
-      }
-
-      return callback(new Error("CORS policy: Origin not allowed"), false);
-    },
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: "*",
+    methods: "*",allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
